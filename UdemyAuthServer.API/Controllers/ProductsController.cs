044d9f17871d081a3a51aa3b92a8ecc464fd7d68
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-26 14:11:06 +0300

    ProductsController created

---
+++ UdemyAuthServer.API/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.DTOs;
+using UdemyAuthServer.Core.Models;
+using UdemyAuthServer.Core.Services;
+
+namespace UdemyAuthServer.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IServiceGeneric<Product, ProductDto> _productService;
+
+        public ProductsController(IServiceGeneric<Product, ProductDto> productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return ActionResultInstance(await _productService.GetAllAsync());
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProducts(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.AddAsync(productDto));
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProducts(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Update(productDto,productDto.Id));
+
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProducts(int id)
+        {
+            return ActionResultInstance(await _productService.Remove(id));
+
+        }
+    }
+}
