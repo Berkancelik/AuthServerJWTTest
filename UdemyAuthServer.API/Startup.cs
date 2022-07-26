56eb58f10afce925853b7d51cdd94bcf9e621131
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-26 16:00:28 +0300

    all api security provided

--- UdemyAuthServer.API/Startup.cs
+++ UdemyAuthServer.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SharedLibrary.Configurations;
+using SharedLibrary.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
